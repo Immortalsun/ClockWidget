8e275daa33755eafbfa747b20fc6db15229f6302
mahinshaw <mahinshaw@gmail.com>
2014-12-04 11:36:18 -0500

    Keep the api key around since its free.

---
+++ ApiKey.cs
@@ -0,0 +1,8 @@
+namespace TimeZoneHelper
+{
+    public static class ApiKey
+    {
+        // http://www.ncdc.noaa.gov/cdo-web/webservices/v2
+        public static string Key { get { return "XLQkvfpMMBYvmwZHPbOUEHFCfZoyDalw"; } }
+    }
+}
