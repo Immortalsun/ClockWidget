42e395660e01256067fdc40e0b7d15d0bb03480a
Immortalsun <maashes.p@gmail.com>
2014-12-04 13:12:04 -0500

    Add thread safety to WeatherRequester

--- WeatherApiConnect/WeatherRequester.cs
+++ WeatherApiConnect/WeatherRequester.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TimeZoneHelper.WeatherApiConnect
@@ -10,17 +11,85 @@ namespace TimeZoneHelper.WeatherApiConnect
     {
         #region Fields
 
+        public static string WeatherRequestUrl =
+            "http://www.ncdc.noaa.gov/cdo-web/api/v2/locations";
+        private readonly object _threadLock = new object();
+        private TimeSpan _updateTime;
+        private List<WeatherUpdater> UpdaterList;
+        private volatile bool Updating;
+
         #endregion
 
         #region Properties
-
+        public bool UpdateSuccessful { get; set; }
         #endregion
 
         #region Constructors
 
+        public WeatherRequester()
+        {
+            UpdaterList = new List<WeatherUpdater>();
+            _updateTime = new TimeSpan(0,15,0);
+        }
+
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Asyc start method for 
+        /// updating weather
+        /// </summary>
+        public async void Start()
+        {
+            Updating = true;
+            while (Updating)
+            {
+                await UpdateWeather();
+                if (UpdateSuccessful)
+                {
+                    lock (_threadLock)
+                    {
+                        Monitor.Wait(_threadLock, _updateTime);
+                    }
+                }
+                else
+                {
+                    Updating = false;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Async update method, 
+        /// actually does requests
+        /// </summary>
+        /// <returns></returns>
+        public async Task UpdateWeather()
+        {
+            UpdateSuccessful = false;
+
+            if (UpdaterList.Any())
+            {
+                foreach (var weatherUpdater in UpdaterList)
+                {
+                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops weather updating thread
+        /// </summary>
+        public void Stop()
+        {
+            Updating = false;
+
+            lock (_threadLock)
+            {
+                Monitor.Pulse(_threadLock);
+            }
+        }
 
         #endregion
 
